0278bcace1407fb47578d388a65a617109a4a14a
Jason Shew <jason@shew.cc>
2025-07-06 16:03:34 -0400

    enabled dynamic site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless'; // or `@astrojs/vercel/edge` if using edge
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+});
