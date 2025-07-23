0a97dcbc2b4e507edf7975dcfd35b3f35e583607
Jason Shew <jason@shew.cc>
2025-07-23 02:12:09 -0400

    updated API routes 7

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,7 @@ const api = axios.create({ baseURL: API_BASE });
 
 
 export const fetchCheckinsHTML = () =>
-  api.get('/fetch-checkins.js', { responseType: 'text' }).then(r => r.data);
+  api.get('/fetch-checkins', { responseType: 'text' }).then(r => r.data);
 export const fetchLinkbook = () =>   api.get('/fetch-linkbook').then(r => normalize(r, ['data', 'rows', 'linkbook']));
 export const fetchTracks = () =>   api.get('/fetch-tracks').then(r => normalize(r, ['data', 'rows', 'tracks']));
 
