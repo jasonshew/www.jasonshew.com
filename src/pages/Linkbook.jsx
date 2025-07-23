81f4f6217cce037e482686f79e7b4beeafa4d03c
Jason Shew <jason@shew.cc>
2025-07-23 02:31:29 -0400

    updated tracks linkbook

--- src/pages/Linkbook.jsx
+++ src/pages/Linkbook.jsx
@@ -1,15 +1,19 @@
 import { Helmet } from 'react-helmet-async';
+import ExternalScript from '../components/ExternalScript.jsx';
 
 export default function Linkbook() {
   return (
     <>
-      <Helmet>
-        <title>Linkbook</title>
-      </Helmet>
-      <section className="linkbook-page prose dark:prose-invert">
-        <h1 className="text-3xl font-bold mb-6">Linkbook</h1>
-        <p></p>
+      <Helmet><title>Linkbook :: Jason Shew</title></Helmet>
+
+      <section id="linkbook" className="prose dark:prose-invert max-w-none">
+        <h1>Linkbook</h1>
+        <div id="loading"><span class="dots">Loading...</span></div>
+        <div id="links-list"></div>
       </section>
+
+      {/* Load the old script AFTER the DOM nodes exist */}
+      <ExternalScript src="https://api.shewtopia.com/jshewblogapi/fetch/fetch-linkbook.js" />
     </>
   );
 }
