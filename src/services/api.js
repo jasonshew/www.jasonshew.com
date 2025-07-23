b0a91834c65b74c029950012d393ea9038326384
Jason Shew <jason@shew.cc>
2025-07-23 01:48:39 -0400

    updated API routes

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || '/api'
+  baseURL: import.meta.env.VITE_API_BASE || 'https://api.shewtopia.com/jshewblogapi/fetch'
 });
 
 // Replace with your real endpoints:
-export const fetchCheckins = () => api.get('/checkins').then(r => r.data);
-export const fetchLinkbook = () => api.get('/linkbook').then(r => r.data);
-export const fetchTracks = () => api.get('/tracks').then(r => r.data);
+export const fetchCheckins = () => api.get('/fetch-checkins').then(r => r.data);
+export const fetchLinkbook = () => api.get('/fetch-linkbook').then(r => r.data);
+export const fetchTracks = () => api.get('/fetch-tracks').then(r => r.data);
 export const fetchPersonality = () => api.get('/personality').then(r => r.data);
 export const fetchUses = () => api.get('/uses').then(r => r.data);
