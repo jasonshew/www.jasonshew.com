125b97f9c5ad5d154b21b347ff0f6d17bb842e10
Jason Shew <jason@shew.cc>
2025-07-23 02:36:34 -0400

    restored legacy css

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App.jsx';
 import './assets/tailwind.css'; // Tailwind
-// import './assets/legacy.css'; // optional: for reference or things not yet converted
+import './assets/legacy.css'; // optional: for reference or things not yet converted
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
